f48dcf8954be776b9ebc3675d17a1546e64c4ab8
achal saharan <asaharan_be17@thapar.edu>
2022-01-08 20:51:17 +0530

    feat: setup router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { Header } from './features/nav';
+import { Sidebar } from './features/nav';
+import { Login } from './features/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-sx">Achal</h1>
-      <Counter />
+    <div style={{ maxWidth: '1500px' }} className="mx-auto">
+      <Header />
+      <div className="grid grid-cols-6">
+        <div className="hidden md:block md:col-span-1 shadow-lg pt-20 min-h-screen bg-purple-700">
+          <Sidebar />
+        </div>
+      </div>
+      <div className="col-span-6 md:col-span-5  mt-20">
+        <Routes>
+          <Route path="/" element={<Counter />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/features/nav/Header.jsx
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+
+import { SidebarModal } from './SidebarModal';
+
+export function Header() {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    return (
+        <>
+            <div className="fixed w-screen top-0 left-0 z-0 border-b-2" style={{backgroundColor: "white"}}>
+                <div
+                    className="mx-auto py-4 px-2 text-purple-600 flex items-center"
+                    style={{ maxWidth: '1500px' }}
+                >
+                    <button
+                        className="mr-4 md:hidden hover:text-purple-800"
+                        onClick={() => setSidebarOpen(true)}
+                    >
+                        <i className="fas fa-bars fa-lg "></i>
+                    </button>
+                    <h1 className="text-lg font-bold">Dukaan</h1>
+                    <div className="flex space-x-4 ml-auto">
+                        {false ? (
+                            <button
+                                className="hover:text-purple-800"
+                                // onClick={() => logoutUser()}
+                            >
+                                Logout
+                            </button>
+                        ) : (
+                            <>
+                                <button
+                                    className="hover:text-purple-800"
+                                >
+                                    Login
+                                </button>
+                                <button
+                                    className="hover:text-purple-800"
+                                >
+                                    Register
+                                </button>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+
+            {sidebarOpen && <SidebarModal setSidebarOpen={setSidebarOpen} />}
+        </>
+    );
+}
